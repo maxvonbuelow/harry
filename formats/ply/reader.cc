b2dd6b45a23b824c3bc01f7109db7b0942d66c6b
Max von Buelow <max@m9x.de>
2017-10-01 17:35:12 +0200

    added bsd license

--- formats/ply/reader.cc
+++ formats/ply/reader.cc
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2017, Max von Buelow
+ * TU Darmstadt - Graphics, Capture and Massively Parallel Computing
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ */
+
 #include "reader.h"
 
 #include <iostream>
